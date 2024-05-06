a11be4bcf2f56cf058c10ce417aec05179e1b093
weedeej <work.deeej@gmail.com>
2024-05-07 06:43:49 +0800

    Update layout to use entire space

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,7 +4,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <main className="container">
+      <main className="container h-full">
         {children}
       </main>
     </div>
